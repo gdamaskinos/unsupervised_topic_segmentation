4f4856d6e33e7d8c1a30ef4bac1d63885973cb3b
gdamaskinos <georgios.damaskinos@gmail.com>
2021-07-06 14:16:49 +0300

    Add hyperparameters

--- types.py
+++ types.py
@@ -14,5 +14,13 @@ class TopicSegmentationAlgorithm(Enum):
     BERT = 2
     SBERT = 3
 
+class TextTilingHyperparameters(NamedTuple):
+    SENTENCE_COMPARISON_WINDOW: int = 15
+    SMOOTHING_PASSES: int = 2
+    SMOOTHING_WINDOW: int = 1
+    TOPIC_CHANGE_THRESHOLD: float = 0.6
+
 class TopicSegmentationConfig(NamedTuple):
-    pass
+    TEXT_TILING: Optional[TextTilingHyperparameters] = None
+    MAX_SEGMENTS_CAP: bool = True
+    MAX_SEGMENTS_CAP__AVERAGE_SEGMENT_LENGTH: int = 60
